7f81fd5e65d216db769212ad3576d39e976058bb
Rishav <rishavkumar005@gmail.com>
2019-05-22 16:26:25 +0530

    init

---
+++ app/src/main/java/rishav/learn/com/blogapp/activity/SplashActivity.java
@@ -0,0 +1,46 @@
+package rishav.learn.com.blogapp.activity;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rishav.learn.com.blogapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    boolean isBackPressed = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new CountDownTimer(2000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+
+            }
+
+            @Override
+            public void onFinish() {
+                if (!isBackPressed) {
+
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    finish();
+
+
+                }
+            }
+        }.start();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        isBackPressed = true;
+
+    }
+
+}
